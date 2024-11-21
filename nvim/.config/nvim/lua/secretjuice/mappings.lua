e09d45602fbf9651b1a245d52c02d83e48bdc3a9
Connor Robb <connor.m.robb33@gmail.com>
2024-11-21 03:42:45 -0700

    vertically shift visual blocks

--- nvim/.config/nvim/lua/secretjuice/mappings.lua
+++ nvim/.config/nvim/lua/secretjuice/mappings.lua
@@ -1,5 +1,14 @@
 vim.g.mapleader = ' '
+
+-- Set Explore
 vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
+
+-- Vertically shift visual blocks
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- XClip-based clipboard provider. It's meant to strip the carriage returns
+-- off, but it's not working. Needs looked into.
 vim.g.clipboard = {
     name = "XClipClipboard",
     copy = {
--- readme.md
+++ readme.md
@@ -5,7 +5,6 @@
 - run the `install.sh` script (might need to give permissions with chmod)
 - fix things along the way, because it's probably pretty broken
 
-
 ## Known Issues and TODOs
 - XClip based Clipboard provider is not striping `\r` from the ends of lines when pasting
 - Configured to use zsh plugins via antibody, but I haven't actually picked any out yet
